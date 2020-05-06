f192d8acf3e7444f15c55ba5a142bc6641d1794e
JustineGoroshenko <justinegoroshenko@yahoo.com>
2020-05-06 22:09:20 +0100

    Advanced12

--- script.js
+++ script.js
@@ -1,53 +1,9 @@
-'use strict';
+'use strict'
+ let button = document.querySelector("#change"),
+     code = document.querySelector("#color");
 
-const isNum = function(n) {
-  return !isNaN(parseFloat(n)) && isFinite(n);
-}
-
-const getRandomNum = function(min, max){
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min +1)) +min;
-}
-
-const getCounter = function(){
-  let counter = 0;
-  return function(){
-    return counter++;
-  }
-};
-const gameRandom = function(attempts) {
-  const randomNum = getRandomNum(1, 100);
-  console.log(randomNum);
-  const counter = getCounter();
-
-  return function checkNumber(){
-    const count = counter();    
-    const userNumber = prompt('Try to guess a number');
-    console.log(count);
-
-    if(isNum(userNumber)){
-      let repeat = false;
-       if (count < attempts){
-        const num = +userNumber;
-          if(num> randomNum) {
-            alert('The number is smaller');
-            return checkNumber();
-          } if(num < randomNum){
-            alert('The number is bigger');
-            return checkNumber();
-          }
-           repeat = confirm('You have won! Want to try again?');
-      } else{
-         repeat = confirm('No attempts left, Like to try again?')
-      }
-      if(repeat) gameRandom(attempts);
-       
-    }else{ 
-      alert('Insert a number!');
-     checkNumber();
-    }
-  }
-}
-const game = gameRandom(10);
-game();
+button.addEventListener("click", function(){
+  let rand = Math.floor(Math.random()*16777215).toString(16);
+  document.body.style.backgroundColor = "#" + rand;
+  code.innerHTML = "#"+rand;
+});
