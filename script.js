aff8900cea41a4d8ea66da87b675eea5af625a63
JustineGoroshenko <justinegoroshenko@yahoo.com>
2020-05-14 14:05:14 +0100

    lesson16

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+class First {
+   hello(){
+      console.log('Hi, I am a method of a parent element');
+   }
+ }
+class Second {
+    constructor() {
+        this.first = new First();
+    }
+    tryingMethod(){
+      this.first.hello();
+      console.log("And I am a child element");
+    }
+ }
+ var child = new Second();
+ child.tryingMethod();
