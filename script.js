c25016caf3161be87ee0d463580d6f22fe9dd4f4
JustineGoroshenko <justinegoroshenko@yahoo.com>
2020-05-15 17:18:42 +0100

    lesson17

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 'use strict';
 
-class First {
-  hello() {
-    return 'I am a method of a parent element' 
-  }
-}
-class Second extends First {
-  show() {
-    return this.hello() + '... and I am a child method ;)';
-  }
-}
-let second = new Second();
-console.log(second.show());
+const myLesson = [
+  {lesson: 1, status: true},
+  {lesson: 2, status: true},
+  {lesson: 3, status: false},
+  {lesson: 4, status: true},
+  {lesson: 5, status: false},
+  {lesson: 6, status: true},
+  {lesson: 7, status: true},
+  {lesson: 8, status: true},
+  {lesson: 9, status: true},
+  {lesson: 10, status: false}
+];
+
+const falseLesson = myLesson.filter( el => el.status === false);
+const secondFalse = myLesson.reduce((a , o) => o.status === false ? a.concat(o.lesson) : a, []);
+
+console.log(falseLesson); ///poluchiaju v array
+console.log(secondFalse); // 
